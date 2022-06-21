5def5f0c7beb96f35bfcc8df4baeb70fc7581b50
Siar Wahidi <siar.wahidi@gmail.com>
2022-06-21 17:46:15 -0400

    Express setup along with customer middleware

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log('04 Store API')
+require("dotenv").config();
+// async errors
+
+const express = require("express");
+const app = express();
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
+});
+
+// products route
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const PORT = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    // connectDB
+    app.listen(PORT, console.log(`Server running on PORT: ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
