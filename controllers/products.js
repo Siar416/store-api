d0927ae6e81cd6644a1619eff69ab9c4bee7ce14
Siar Wahidi <siar.wahidi@gmail.com>
2022-06-22 13:30:42 -0400

    Added functionality to display only selected fields. Added functionality for user to limit items along with pagination functionality

--- controllers/products.js
+++ controllers/products.js
@@ -8,7 +8,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -25,6 +25,8 @@ const getAllProducts = async (req, res) => {
 
   // console.log(queryObject);
   let result = Product.find(queryObject);
+
+  // sort
   if (sort) {
     const sortList = sort.split(",").join(" ");
     result = result.sort(sortList);
@@ -32,6 +34,18 @@ const getAllProducts = async (req, res) => {
   } else {
     result = result.sort("createdAt");
   }
+
+  if (fields) {
+    const fieldList = fields.split(",").join(" ");
+    result = result.select(fieldList);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
