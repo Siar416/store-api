69caa2dc79a4dafcdcd0bac9a156a5fa0a97ad9d
Siar Wahidi <siar.wahidi@gmail.com>
2022-06-22 11:38:49 -0400

    Created populate file to dynamically populate db with products

---
+++ populate.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+const connectDB = require("./db/connect");
+const Product = require("./models/product");
+
+const jsonProducts = require("./products.json");
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    // Product.deleteMany() removes any previous data from db before we populate it with the json products array
+    await Product.deleteMany();
+    await Product.create(jsonProducts);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
+
+// need to stop server then run code; node populate
